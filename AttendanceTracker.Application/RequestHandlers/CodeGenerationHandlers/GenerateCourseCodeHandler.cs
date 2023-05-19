fe1bcc563832344c14e2cc779dce0b6e18400bee
Daniel Aguirre <uhyeay24@gmail.com>
2023-05-19 06:50:00 -0500

    Add logic for restricting CourseCode to within Maximum Length

--- AttendanceTracker.Application/RequestHandlers/CodeGenerationHandlers/GenerateCourseCodeHandler.cs
+++ AttendanceTracker.Application/RequestHandlers/CodeGenerationHandlers/GenerateCourseCodeHandler.cs
@@ -4,13 +4,13 @@ namespace AttendanceTracker.Application.RequestHandlers.CodeGenerationHandlers
 {
     public class GenerateCourseCodeRequest : CodeGenerationRequest, IValidatable
     {
-        public GenerateCourseCodeRequest(string courseSubjectCode) => CourseSubjectCode = courseSubjectCode;
+        public GenerateCourseCodeRequest(string courseSubjectCode) => SubjectCode = courseSubjectCode;
 
-        public string CourseSubjectCode { get; set; } = null!;
+        public string SubjectCode { get; set; } = null!;
 
         public bool IsValid(out List<string> validationFailures) => 
             Validation.Initialize(out validationFailures)
-                .AddFailureIfNullOrWhiteSpace(CourseSubjectCode, nameof(CourseSubjectCode))
+                .AddFailureIfNullOrWhiteSpace(SubjectCode, nameof(SubjectCode))
             .IsValidWhenNoFailures();
     }
 
@@ -20,12 +20,19 @@ namespace AttendanceTracker.Application.RequestHandlers.CodeGenerationHandlers
         {
             var randomDigits = new char[CourseCodeConstants.NumberOfEndingDigits];
 
+            var maxLengthForSubjectCode = CourseCodeConstants.MaxLength - CourseCodeConstants.NumberOfEndingDigits;
+
+            if (request.SubjectCode.Length > maxLengthForSubjectCode)
+            {
+                request.SubjectCode = request.SubjectCode[..maxLengthForSubjectCode];
+            }
+
             for (int i = 0; i < randomDigits.Length; i++)
             {
                 randomDigits[i] = GetRandomNumber();
             }
 
-            return request.CourseSubjectCode + new string(randomDigits);
+            return request.SubjectCode + new string(randomDigits);
         }
     }
 }
