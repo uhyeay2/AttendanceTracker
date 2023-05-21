0c40a06ef6c570e1c9b8215ff6258a9d530cf859
Daniel Aguirre <uhyeay24@gmail.com>
2023-05-20 21:39:33 -0500

    add IsSubjectCodeExisting DataRequest with Test

---
+++ AttendanceTracker.Data.Tests/DataRequestTests/SubjectTests/IsSubjectCodeExistingTests.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceTracker.Data.DataRequestObjects.SubjectRequests;
+
+namespace AttendanceTracker.Data.Tests.DataRequestTests.SubjectTests
+{
+    public class IsSubjectCodeExistingTests : DataTest
+    {
+        [Fact]
+        public async Task IsSubjectCodeExisting_Given_CodeNotExisting_ShouldReturn_False()
+        {
+            Assert.False(await _dataAccess.FetchAsync(new IsSubjectCodeExisting(RandomString())));
+        }
+
+        [Fact]
+        public async Task IsSubjectCodeExisting_Given_CodeIsExisting_ShouldReturn_True()
+        {
+            var subject = await _dataSeeder.NewSubject();
+
+            Assert.True(await _dataAccess.FetchAsync(new IsSubjectCodeExisting(subject.SubjectCode)));
+        }
+    }
+}
---
+++ AttendanceTracker.Data/DataRequestObjects/SubjectRequests/IsSubjectCodeExisting.cs
@@ -0,0 +1,9 @@
+﻿namespace AttendanceTracker.Data.DataRequestObjects.SubjectRequests
+{
+    public class IsSubjectCodeExisting : Code_DataRequest<bool>
+    {
+        public IsSubjectCodeExisting(string code) : base(code) { }
+
+        public override string GetSql() => Select.Exists(TableNames.Subject, where: "SubjectCode = @Code");
+    }
+}
